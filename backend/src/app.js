b8613caa024fc112a0dd2f075e89d1e2ad3ef6c0
smckenziebsu <scotty.mckenzie@bsu.edu>
2025-08-28 18:10:06 +0000

    chore: building api routes with express

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,10 @@
 import express from 'express'
+import { postsRoutes } from './routes/posts.js'
+import bodyParser from 'body-parser'
 const app = express()
+app.use(bodyParser.json())
+postsRoutes(app)
 app.get('/', (req, res) => {
-  res.send('Hello from Express Live!')
+  res.send('Hello from Express!')
 })
 export { app }
