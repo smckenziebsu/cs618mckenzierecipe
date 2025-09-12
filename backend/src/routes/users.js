36f62184f0535537a0eb6a27227cfb22b399bacc
smckenziebsu <scotty.mckenzie@bsu.edu>
2025-09-12 19:05:59 +0000

    chore: implement routes in backend

---
+++ backend/src/routes/users.js
@@ -0,0 +1,13 @@
+import { createUser } from '../services/users.js'
+export function userRoutes(app) {
+  app.post('/api/v1/user/signup', async (req, res) => {
+    try {
+      const user = await createUser(req.body)
+      return res.status(201).json({ username: user.username })
+    } catch (err) {
+      return res.status(400).json({
+        error: 'failed to create the user, does the username already exist?',
+      })
+    }
+  })
+}
