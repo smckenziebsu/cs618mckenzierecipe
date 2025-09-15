d5a9909e23295be706407bf90d2688c73bb86cfa
smckenziebsu <scotty.mckenzie@bsu.edu>
2025-09-15 14:53:07 +0000

    chore: define authenticated routes

--- backend/src/routes/posts.js
+++ backend/src/routes/posts.js
@@ -7,6 +7,7 @@ import {
   deletePost,
   getPostById,
 } from '../services/posts.js'
+import { requireAuth } from '../middleware/jwt.js'
 export function postsRoutes(app) {
   app.get('/api/v1/posts', async (req, res) => {
     const { sortBy, sortOrder, author, tag } = req.query
@@ -39,7 +40,7 @@ export function postsRoutes(app) {
       return res.status(500).end()
     }
   })
-  app.post('/api/v1/posts', async (req, res) => {
+  app.post('/api/v1/posts', requireAuth, async (req, res) => {
     try {
       const post = await createPost(req.body)
       return res.json(post)
@@ -48,7 +49,7 @@ export function postsRoutes(app) {
       return res.status(500).end()
     }
   })
-  app.patch('/api/v1/posts/:id', async (req, res) => {
+  app.patch('/api/v1/posts/:id', requireAuth, async (req, res) => {
     try {
       const post = await updatePost(req.params.id, req.body)
       return res.json(post)
@@ -57,7 +58,7 @@ export function postsRoutes(app) {
       return res.status(500).end()
     }
   })
-  app.delete('/api/v1/posts/:id', async (req, res) => {
+  app.delete('/api/v1/posts/:id', requireAuth, async (req, res) => {
     try {
       const { deletedCount } = await deletePost(req.params.id)
       if (deletedCount === 0) return res.sendStatus(404)
