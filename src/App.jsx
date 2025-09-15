4616a4bcec763b65809a6f69638725b109cd6a32
smckenziebsu <scotty.mckenzie@bsu.edu>
2025-09-15 20:30:04 +0000

    chore: login and logout in frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Blog } from './pages/Blog.jsx'
 import { Signup } from './pages/Signup.jsx'
+import { Login } from './pages/Login.jsx'
+import { AuthContextProvider } from './contexts/AuthContext.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 const queryClient = new QueryClient()
 const router = createBrowserRouter([
- {
+  {
     path: '/',
     element: <Blog />,
   },
@@ -12,11 +14,17 @@ const router = createBrowserRouter([
     path: '/signup',
     element: <Signup />,
   },
+  {
+    path: '/login',
+    element: <Login />,
+  },
 ])
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <AuthContextProvider>
+        <RouterProvider router={router} />
+      </AuthContextProvider>
     </QueryClientProvider>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,22 @@
 import { Link } from 'react-router-dom'
+import { jwtDecode } from 'jwt-decode'
+import { useAuth } from '../contexts/AuthContext.jsx'
 export function Header() {
+  const [token, setToken] = useAuth()
+  if (token) {
+    const { sub } = jwtDecode(token)
+    return (
+      <div>
+        Logged in as <b>{sub}</b>
+        <br />
+        <button onClick={() => setToken(null)}>Logout</button>
+      </div>
+    )
+  }
   return (
     <div>
-          <h1>Welcome to my Blog</h1>
-      <Link to='/signup'>Sign Up</Link>
-      <br />
+      <h1> Welcome to my Blog!</h1>
+      <Link to='/login'>Log In</Link> | <Link to='/signup'>Sign Up</Link>
     </div>
   )
 }
