f30230143ede16894735c049c0b687519074b758
smckenziebsu <scotty.mckenzie@bsu.edu>
2025-09-23 15:55:15 +0000

    chore: updates to recipe frontend

--- src/components/CreateRecipe.jsx
+++ src/components/CreateRecipe.jsx
@@ -1,22 +1,28 @@
 import { useState } from 'react'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useAuth } from '../contexts/AuthContext.jsx'
-import { createPost } from '../api/posts.js'
-export function CreatePost() {
+import { createPost } from '../api/recipes.js'
+
+export function CreateRecipe() {
   const [title, setTitle] = useState('')
   const [contents, setContents] = useState('')
   const [token] = useAuth()
-  // REMOVE author
+
   const queryClient = useQueryClient()
-  const createPostMutation = useMutation({
-    mutationFn: () => createPost(token, { title, contents }), // REMOVE AUTHOR
-    onSuccess: () => queryClient.invalidateQueries(['posts']),
+  const createRecipeMutation = useMutation({
+       mutationFn: () =>
+      createRecipe(token, {
+        title,
+        ingredients: ingredients.split(',').map((i) => i.trim()),
+      }),
+    onSuccess: () => queryClient.invalidateQueries(['recipes']),
   })
+  
   const handleSubmit = (e) => {
     e.preventDefault()
-    createPostMutation.mutate()
+    createRecipeMutation.mutate()
   }
-  if (!token) return <div>Please log in to create new posts.</div>
+  if (!token) return <div>Please log in to create new recipes.</div>
   return (
     <form onSubmit={handleSubmit}>
       <div>
@@ -30,18 +36,19 @@ export function CreatePost() {
         />
       </div>
       <br />
-
-      <textarea
-        value={contents}
-        onChange={(e) => setContents(e.target.value)}
-      />
       <br />
       <br />
+      <label htmlfFor="create-ingredients">Ingreients (comma seperated): </label>
       <input
-        type='submit'
-        value={createPostMutation.isPending ? 'Creating....' : 'Create'}
-        disabled={!title}
+        type='text'
+        name="create-title"
+        id="create-title"
+        value={ingredients}
+        onChange={(e) => setIngredients(e.target.value)}
+        placeholder="e.g. chicken, rice"
       />
+
+      
       {createPostMutation.isSuccess ? (
         <>
           <br />
